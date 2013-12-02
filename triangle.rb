2f64b5149f5703ec0e16bb0c3147df29c32116f6
Dave Filion <dave.filion@gmail.com>
2013-12-01 19:15:27 -0500

    more

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,23 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	sides = [a, b, c]
+
+	if sides.any? {|side| side == 0} 
+		raise TriangleError, "can't have 0 length side"
+	end
+
+	if sides.any? { |side| side < 0}
+		raise TriangleError, "can't have side length < 0"
+	end
+
+	if sides.uniq.count == 1
+		:equilateral
+	elsif sides.uniq.count == 2
+		:isosceles
+	else
+		:scalene
+	end
 end
 
 # Error class used in part 2.  No need to change this code.
